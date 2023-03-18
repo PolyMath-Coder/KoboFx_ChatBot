17cbb93b8c902d25d287b160056545678a101ea8
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-03-18 13:28:43 +0100

    set up logger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+require('dotenv').config();
+const { PORT } = require('./config/keys');
+const logger = require('./config/logger');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: true });
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server is now live at port ${PORT}`);
+});
